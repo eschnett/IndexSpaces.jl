d702d02be33fe5098d423f4afd90796702d4cf69
Erik Schnetter <schnetter@gmail.com>
2023-02-14 15:34:27 -0500

    upchan: Add documentation

--- kernels/upchan.cxx
+++ kernels/upchan.cxx
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <cmath>
 #include <complex>
 #include <iostream>
 #include <vector>
@@ -59,14 +60,16 @@ constexpr std::complex<T> convert(const std::complex<I> i) {
 
 template <typename I, typename T>
 constexpr I quantize(const T x, const I imax) {
-  const I itmp = (I)(x + T(0.5));
+  using std::floor;
+  const I itmp = I(floor(x + T(0.5)));
   using std::max, std::min;
   const I i = min(imax, max(I(-imax), itmp));
   return i;
 }
 template <typename I, typename T>
 constexpr std::complex<I> quantize(const std::complex<T> x, const I imax) {
-  return std::complex<I>(quantize(x.real(), imax), quantize(x.imag(), imax));
+  return std::complex<I>(quantize<I>(x.real(), imax),
+                         quantize<I>(x.imag(), imax));
 }
 
 // complex numbers
@@ -80,17 +83,20 @@ constexpr std::array<T, 2> to_array(const std::complex<T> c) {
   return std::array<T, 2>{c.imag(), c.real()};
 }
 
+// functions
+template <typename T> constexpr T sinc(const T x) {
+  return x == T(0) ? T(1) : sin(x) / x;
+}
+
 // array indexing
 
-constexpr int Widx(int m, int u) { return m + M * u; }
-
-constexpr int Gidx(int u) { return u; }
-
 constexpr int Eidx(int c, int d, int f, int p, int t) {
+  assert(t >= 0 && t < T + M * U - 1);
   return d + D * f + D * F * p + D * F * P * t;
 }
 
 constexpr int Ebaridx(int c, int d, int fbar, int p, int tbar) {
+  assert(tbar >= 0 && tbar < T / U);
   return d + D * fbar + D * F * U * p + D * F * U * P * tbar;
 }
 
@@ -116,12 +122,16 @@ void upchan_simple(const float16_t *__restrict__ const W,
 
               const float W1 = W[s];
 
+              using std::polar;
+              const std::complex<float> phase1 =
+                  polar(1.0f, float(M_PI) * s * (U - 1) / U);
+              const std::complex<float> phase2 =
+                  polar(1.0f, -2 * float(M_PI) * u * s / U);
+
               const std::complex<float> E1 =
                   convert<float>(to_complex(get4(E[Eidx(0, d, f, p, t)])));
 
-              using std::polar;
-              Ebar1 += W1 * polar(1.0f, float(M_PI) * s * (U - 1) / U) *
-                       polar(1.0f, -2 * float(M_PI) * u * s / U) * E1;
+              Ebar1 += W1 * phase1 * phase2 * E1;
 
             } // s
 
@@ -143,21 +153,36 @@ int main(int argc, char **argv) {
   std::cout << "CHORD upchannelization kernel\n";
 
   std::cout << "Initializing input...\n";
-  std::vector<float16_t> W(M * U);                // PFB weight function
-  std::vector<float16_t> G(U);                    // output gains
-  std::vector<int4x2_t> E(C * D * F * P * T / 2); // electric field
+  std::vector<float16_t> W(M * U); // PFB weight function
+  std::vector<float16_t> G(U);     // output gains
+  std::vector<int4x2_t> E(C * D * F * P * (T + M * U - 1) /
+                          2); // electric field
   std::vector<int4x2_t> Ebar(C * D * F * P * T /
                              2); // upchannelized electric field
+  using std::cos, std::pow, std::sin;
+  for (int s = 0; s < M * U; ++s)
+    // sinc-Hanning weight function, eqn. (11), with `N=U`, and with `M*U`
+    // changed to `M*U-1`
+    W.at(s) =
+        pow(cos(float(M_PI) * (s - (M * U - 1) / 2.0f) / (M * U - 1)), 2) *
+        sinc((s - (M * U - 1) / 2.0f) / U);
   for (int u = 0; u < U; ++u)
-    for (int m = 0; m < M; ++m)
-      W.at(Widx(m, u)) = 0;
-  for (int u = 0; u < U; ++u)
-    G.at(Gidx(u)) = 0;
-  for (int t = 0; t < T; ++t)
-    for (int p = 0; p < P; ++p)
-      for (int f = 0; f < F; ++f)
-        for (int d = 0; d < D; ++d)
-          E.at(Eidx(0, d, f, p, t)) = set4(0, 0);
+    G.at(u) = 1;
+  for (int t = 0; t < T + M * U - 1; ++t) {
+    for (int p = 0; p < P; ++p) {
+      for (int f = 0; f < F; ++f) {
+        for (int d = 0; d < D; ++d) {
+          using std::polar;
+          // const std::complex<float> E1 = 0.0f;
+          // const std::complex<float> E1 = 1.0f;
+          // const std::complex<float> E1 =
+          //     polar(7.0f, 2 * float(M_PI) * t / (T / U));
+          const std::complex<float> E1 = t == 0 ? 1.0f : 0.0f;
+          E.at(Eidx(0, d, f, p, t)) = set4(to_array(quantize<int8_t>(E1, 7)));
+        }
+      }
+    }
+  }
   for (int tbar = 0; tbar < T / U; ++tbar)
     for (int p = 0; p < P; ++p)
       for (int fbar = 0; fbar < F * U; ++fbar)
@@ -168,17 +193,24 @@ int main(int argc, char **argv) {
   upchan_simple(W.data(), G.data(), E.data(), Ebar.data());
 
   std::cout << "Checking output...\n";
-  for (int tbar = 0; tbar < T / U; ++tbar) {
+  int num_errors = 0;
+  for (int fbar = 0; fbar < F * U; ++fbar) {
     for (int p = 0; p < P; ++p) {
-      for (int fbar = 0; fbar < F * U; ++fbar) {
-        for (int d = 0; d < D; ++d) {
-          const std::complex<int8_t> Ebar1 =
-              to_complex(get4(Ebar.at(Eidx(0, d, fbar, p, tbar))));
-          assert(Ebar1 == std::complex<int8_t>(0, 0));
+      for (int d = 0; d < D; ++d) {
+        for (int tbar = 0; tbar < T / U; ++tbar) {
+          const std::complex<int> Ebar1 = std::complex<int>(
+              to_complex(get4(Ebar.at(Eidx(0, d, fbar, p, tbar)))));
+          if (Ebar1 != 0) {
+            std::cout << "  d=" << d << " fbar=" << fbar << " p=" << p
+                      << " tbar=" << tbar << " Ebar=" << Ebar1 << "\n";
+            ++num_errors;
+          }
         }
       }
     }
   }
+  if (num_errors > 0)
+    std::cout << "ERRORS FOUND: " << num_errors << "\n";
 
   std::cout << "Done.\n";
   return 0;
