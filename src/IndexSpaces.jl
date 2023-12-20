5c810000179733420c3045995b579842066d93ee
Erik Schnetter <schnetter@gmail.com>
2023-12-20 15:25:15 -0500

    mma_sp: Correct sparsity selector

--- src/IndexSpaces.jl
+++ src/IndexSpaces.jl
@@ -2614,9 +2614,9 @@ function mma_sp_row_col_m16n8k16_f16!(
             @assert c3 == 4
             return (c1, c2)
         end
-        a_row_patterns = NTuple{2,Int}[decode_row_pattern(row, col) for col in 0:4:15 for row in 0:15]
+        a_row_patterns = NTuple{2,Int}[decode_row_pattern(row, col) for row in 0:15 for col in 0:4:15]
         @show a_row_patterns
-        e = [Int16x2(reinterpret(Int, vec(a_row_patterns[threadgroup+1:threadgroup+4,:]))...) for threadgroup in 0:4:31]
+        e = [Int16x2(reinterpret(Int, vec(a_row_patterns[thread+1:8:31,:]))...) for thread in 0:8:31]
         @show e
         push!(
             emitter.statements,
