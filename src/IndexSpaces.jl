300d0fda0cc288eca12f8b50179ba9a3c0d8590a
Erik Schnetter <schnetter@gmail.com>
2024-01-16 19:26:26 -0500

    Correct type assertion

--- src/IndexSpaces.jl
+++ src/IndexSpaces.jl
@@ -2699,7 +2699,7 @@ function mma_sp_row_col_m16n8k16_f16!(
                     # else
                     #     @assert false
                     end
-                    IndexSpaces.mma_sp_m16n8k16(($A0_name, $A1_name)::NTuple{2,Float16x2}, ($B0_name, $B1_name)::NTuple{2,Float16x2}, ($C0_name, $C1_name)::NTuple{2,Float16x2}, e::Int32, 0i32::Int32)::NTuple{2,Float16x2}
+                    IndexSpaces.mma_sp_m16n8k16(($A0_name, $A1_name)::NTuple{2,Float16x2}, ($B0_name, $B1_name)::NTuple{2,Float16x2}, ($C0_name, $C1_name)::NTuple{2,Float16x2}, e::Int2x16, 0i32::Int32)::NTuple{2,Float16x2}
                 end
             end,
         )
