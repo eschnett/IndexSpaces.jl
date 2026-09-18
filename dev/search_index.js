d167a6c15db2a5785f51f88a3e981ae54cf9d712
Documenter.jl <documenter@juliadocs.github.io>
2026-09-18 14:38:56 +0000

    build based on 74bc71d

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#IndexSpaces.jl","page":"IndexSpaces.jl","title":"IndexSpaces.jl","text":"","category":"section"}]
+[{"category":"section","location":"#IndexSpaces.jl","page":"IndexSpaces.jl","text":"","title":"IndexSpaces.jl"}]
 }
